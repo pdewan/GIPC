a51564c1e9d98a4f4861399c788bf7b3c2dc562f
pdewan <dewan@cs.unc.edu>
2014-06-09 02:25:40 -0400

    Made the registry started depend on semaphore rather than console input.
    Added some comments to make it more similar to the non distributed
    assignment on which it is based

--- src/examples/rmi/arithmetic/DemoerOfMixedTypeArithmetic.java
+++ src/examples/rmi/arithmetic/DemoerOfMixedTypeArithmetic.java
@@ -1,7 +1,27 @@
 package examples.rmi.arithmetic;
 
 import bus.uigen.models.MainClassLaunchingUtility;
-
+// this is a distrbuted implementation of a CS 1 (UNC Comp 110) first assignment submission described below
+/******************************************************************
+ * Program or Assignment #: Assignment1
+ *
+ * Programmer: Jacob
+ *
+ * Due Date: Tuesday, Jan. 28
+ *
+ * COMP110-002, Spring 2014       Instructor: Prof. Jay Aikat
+ *
+ * Pledge: I have neither given nor received unauthorized aid
+ *         on this program. 
+ *
+ * Description: Insert a brief paragraph describing the program
+ *
+ * Input: Insert a brief description of user inputs, or "None" if
+ *        there is no user input
+ *
+ * Output: Insert a brief description of the program output
+ *
+ ******************************************************************/
 public class DemoerOfMixedTypeArithmetic {
 	public static void main(String args[]) {
 		demo();
--- src/examples/rmi/arithmetic/DistributedRMIRegistryStarter.java
+++ src/examples/rmi/arithmetic/DistributedRMIRegistryStarter.java
@@ -1,6 +1,7 @@
 package examples.rmi.arithmetic;
 import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 import util.annotations.Tags;
 
@@ -9,8 +10,9 @@ public class DistributedRMIRegistryStarter {
 	public static void main (String[] args) {
 		try {
 			LocateRegistry.createRegistry(1099);
-			Scanner scanner = new Scanner(System.in);
-			scanner.nextLine();
+			(new Semaphore(0)).acquire(); // wait for ever to avoid termination
+//			Scanner scanner = new Scanner(System.in);
+//			scanner.nextLine();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
