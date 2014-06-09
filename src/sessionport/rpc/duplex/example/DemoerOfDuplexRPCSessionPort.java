bef84700ed959c6d9ab6f1988ea24abee7fac7ce
pdewan <dewan@cs.unc.edu>
2014-06-09 03:09:02 -0400

    Got an RPC MVC session port example to work but not an an old adder example

---
+++ src/sessionport/rpc/duplex/example/DemoerOfDuplexRPCSessionPort.java
@@ -0,0 +1,29 @@
+package sessionport.rpc.duplex.example;
+
+import port.sessionserver.ASessionServerLauncher;
+import port.sessionserver.relay.ASessionServerRelayerLauncher;
+import port.sessionserver.relay.ARelayerSupportingSessionServerLauncher;
+import port.sessionserver.relay.SessionServerRelayerLauncher;
+import sessionport.datacomm.group.object.relayed.example.CathyClientRelayedGroupSessionPort;
+import sessionport.datacomm.group.object.relayed.latecomer.ASessionPortLatecomerSupportingSessionServerLauncher;
+import sessionport.datacomm.group.object.relayed.latecomer.example.AliceMemberLatecomerGroupSessionPort;
+import sessionport.datacomm.group.object.relayed.latecomer.example.BobServerLatecomerGroupSessionPort;
+import bus.uigen.models.MainClassLaunchingUtility;
+// this is old stuff, need some kind of add method
+// Need to inheit the lanunchers otherwise the relayer proxy does not work
+public class DemoerOfDuplexRPCSessionPort {
+	public static void main(String args[]) {
+		demo();
+	}	
+	public static void demo() {		
+		Class[] classes = {
+//				SessionPortSessionServerLauncher.class,
+				ASessionPortLatecomerSupportingSessionServerLauncher.class,
+				AnAliceDuplexRPCSessionPort.class,
+				ABobDuplexRPCSessionPort.class,
+				ACathyDuplexRPCSessionPort.class,
+		};
+		MainClassLaunchingUtility.interactiveLaunch(classes);
+	}	
+
+}
---
+++ src/sessionport/rpc/group/mvc/direct/example/DemoerOfMVCDirecRPCGroupSessionPort.java
@@ -0,0 +1,23 @@
+package sessionport.rpc.group.mvc.direct.example;
+
+import port.sessionserver.ASessionServerLauncher;
+import bus.uigen.models.MainClassLaunchingUtility;
+
+public class DemoerOfMVCDirecRPCGroupSessionPort {
+	public static void main(String args[]) {
+		demo();
+	}	
+	public static void demo() {		
+		Class[] classes = {
+//				SessionPortSessionServerLauncher.class,
+//				ASessionPortLatecomerSupportingSessionServerLauncher.class,
+				ASessionServerLauncher.class, // this is direct, so why use the other more complicated session servers
+				ADirectSessionPortMVCServerLauncher.class,
+				AnAliceMVCDirectSessionPortLauncher.class,
+				ABobMVCDirectSessionPortLauncher.class
+				
+		};
+		MainClassLaunchingUtility.interactiveLaunch(classes);
+	}	
+
+}
