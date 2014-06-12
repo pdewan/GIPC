d4baa72e4de3519453c7c119451831ecbaada831
pdewan <dewan@cs.unc.edu>
2014-06-12 14:39:13 -0400

    Static and multi server demos had been swapped, corrected them

---
+++ src/multiserverport/rpc/group/example/DemoerOfRPCGroupMultiServerPort.java
@@ -0,0 +1,32 @@
+package multiserverport.rpc.group.example;
+
+import port.sessionserver.relay.late.ALatecomerSessionServerLauncher;
+import bus.uigen.models.MainClassLaunchingUtility;
+
+public class DemoerOfRPCGroupMultiServerPort {
+	public static void main(String args[]) {
+		demo();
+	}	
+	/*
+	 * This is much changed fromw what was there before, which probably was all wrong.
+	 * The clients join as client only and not symmetric join as they do not serve methods and also
+	 * as they do not have ids, which even the original did not provide. They also do not register 
+	 * remote objects.
+	 */
+	public static void demo() {		
+		Class[] classes = {
+				
+				// all of the clients are clients regaredless of the join choice in the descriptions
+				// because port is explicitly created
+
+				Server1GroupRPCMSPServerLauncher.class, 
+				Server2GroupRPCMSPServerLauncher.class,
+				AliceGroupRPCMSPLauncher.class, 
+				BobGroupRPCMSPLauncher.class,
+
+				
+		};
+		MainClassLaunchingUtility.interactiveLaunch(classes);
+	}	
+
+}
