a9e081c0e8e208d8d6d3f22bf7e8678d6a17185e
pdewan <dewan@cs.unc.edu>
2015-07-28 12:20:24 -0400

    Added a david example session port

---
+++ src/sessionport/rpc/group/mvc/flexible/example/DavidSessionPort.java
@@ -0,0 +1,7 @@
+package sessionport.rpc.group.mvc.flexible.example;
+
+
+public interface DavidSessionPort {
+	public static final String DAVID_ID = "9103";
+	public static final String DAVID_NAME = "David";
+}
