15bdeae92998721ffa42b1b056410eb54d8261c2
pdewan <dewan@cs.unc.edu>
2018-02-07 09:57:14 -0500

    String parsing of method calls

--- src/util/interactiveMethodInvocation/ACommandToMethodCallTranslator.java
+++ src/util/interactiveMethodInvocation/ACommandToMethodCallTranslator.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import assignments.util.inputParameters.SimulationParametersListener;
 /**
@@ -21,8 +23,17 @@ public abstract class ACommandToMethodCallTranslator implements CommandToMethodC
 	public static String QUIT_COMMAND = "quit";
 	
 	
-	public static boolean parseBoolean(String aValue) {
-		return ("true".startsWith(aValue.toLowerCase()));
+//	public  boolean parseBoolean(String aValue) {
+//		return ("true".startsWith(aValue.toLowerCase()));
+//	}
+	public static String parseString(String aValue) {
+		if (aValue.length() < 2 || 
+				(aValue.charAt(0) != '"') ||
+				aValue.charAt(aValue.length()-1) != '"') {
+			System.out.println(aValue + "does not ssurrounding quotes");
+			
+		}
+		return aValue.substring(1, aValue.length()-1);
 	}
 	@Override
 	public <T>  T parseEnum(Class<T> anEnumClass, String aValue) {
@@ -49,7 +60,9 @@ public abstract class ACommandToMethodCallTranslator implements CommandToMethodC
 			return aValue.charAt(0);
 		if (aClass.isEnum())
 			return parseEnum(aClass, aValue);
-		System.err.println(aClass.getSimpleName() + " is not parsable class");
+		if (aClass == String.class)
+			return parseString(aValue);
+		System.err.println(aClass.getSimpleName() + " is not a parsable class");
 		return null;
 	}
 	
@@ -104,6 +117,9 @@ public abstract class ACommandToMethodCallTranslator implements CommandToMethodC
 		String nextLine = scanner.nextLine();
 		return nextLine.split("\\s+");		
 	}
+//	Pattern whiteSpace = Pattern.compile("\\s+");
+	Pattern whiteSpace = Pattern.compile("\\s|$");
+
 	@Override
 	public boolean isPrintCommand(String[] aTokens) {		
 		return PRINT_COMMAND.startsWith(aTokens[0].toLowerCase());
@@ -112,13 +128,37 @@ public abstract class ACommandToMethodCallTranslator implements CommandToMethodC
 	public  boolean isQuitCommand(String[] aTokens) {		
 		return QUIT_COMMAND.startsWith(aTokens[0].toLowerCase());
 	}
+	public static int indexOf(Pattern pattern, String s) {
+	    Matcher matcher = pattern.matcher(s);
+	    return matcher.find() ? matcher.start() : -1;
+	}
 	protected abstract void callMethod(Method aMethod, Object[] anArgs);
 	@Override
 	public void processCommands(Class aClass) {
 		while (true) {
 			System.out.println("Enter (prefixes of) print, quit or a method and its parameters separated by whitespace:");
 			String nextLine = scanner.nextLine();
-			String[] aTokens = nextLine.split("\\s+");
+			int aSpaceIndex = indexOf(whiteSpace, nextLine);
+			if (aSpaceIndex == -1) {
+				System.out.println("Method name missing:" + nextLine);
+				continue;
+			}
+			String aParamString = nextLine.substring(aSpaceIndex);
+			String aCommand = nextLine.substring(0, aSpaceIndex);
+//			String[] aTokens = nextLine.split("\\s+");
+			String[] aParamTokens;
+			if (aParamString.trim().isEmpty()) {
+				aParamTokens = new String[0];
+			} else {
+				aParamTokens = aParamString.split(",");
+			}
+			String[] aTokens = new String[aParamTokens.length+1];
+			aTokens[0] = aCommand.trim();
+			for (int i = 0; i < aParamTokens.length; i++ ) {
+				aTokens[i+1] = aParamTokens[i].trim();
+			}
+			
+
 			if (isQuitCommand(aTokens)) {
 				System.out.println("Exiting");
 				System.exit(0);
