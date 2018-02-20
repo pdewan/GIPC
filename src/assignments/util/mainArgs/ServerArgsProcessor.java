256bbd204ef839f41de2afaee8fc4a340495221e
pdewan <dewan@cs.unc.edu>
2018-02-20 12:19:08 -0500

    Added registry host arg to serverargs processor

--- src/assignments/util/mainArgs/ServerArgsProcessor.java
+++ src/assignments/util/mainArgs/ServerArgsProcessor.java
@@ -17,9 +17,20 @@ package assignments.util.mainArgs;
  */
 public class ServerArgsProcessor {
 	public static final int PORT_ARG_INDEX = 0;	
+	public static final int REGISTRY_HOST_ARG_INDEX = 1;
+
 	public static int getServerPort(String[] args){
 		return args.length > PORT_ARG_INDEX ?
 				Integer.parseInt(args[PORT_ARG_INDEX]):
 					ServerPort.SERVER_PORT;
 	}	
+	/**
+	 * Extracts the server hostname from argument #0,  if it exists, returns default
+	 * hostname (localhost) otherwise
+	 */
+	public static String getRegistryHost(String[] args){
+		return args.length > REGISTRY_HOST_ARG_INDEX?
+				args[REGISTRY_HOST_ARG_INDEX]:
+				"localhost";
+	}
 }
