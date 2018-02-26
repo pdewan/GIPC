e7be71ead268f4f22a94d62ea0b6b9e866692d10
pdewan <dewan@cs.unc.edu>
2018-02-26 17:32:44 -0500

    New consensus trace

---
+++ src/util/trace/port/consensus/ActionWhileEnablingProposalIsPending.java
@@ -0,0 +1,29 @@
+package util.trace.port.consensus;
+
+import inputport.rpc.RemoteCall;
+import util.annotations.ComponentWidth;
+import util.annotations.DisplayToString;
+import util.trace.TraceableInfo;
+@DisplayToString(true)
+@ComponentWidth(1000)
+public class ActionWhileEnablingProposalIsPending extends TraceableInfo {
+	
+
+	public ActionWhileEnablingProposalIsPending(String aMessage, Object aSource, String anObjectName, float aProposalNumber, Object anAction) {
+		super(aMessage, aSource );
+	}
+	
+	
+	public static ActionWhileEnablingProposalIsPending newCase(Object aSource, String anObjectName, float aProposalNumber, Object anAction) {
+    	String aMessage =  anObjectName + "," + aProposalNumber + "=" + anAction;
+    	ActionWhileEnablingProposalIsPending retVal = new ActionWhileEnablingProposalIsPending(aMessage, aSource, anObjectName, aProposalNumber, anAction);
+   	    retVal.announce();
+    	return retVal;
+
+	}
+	
+	static {
+//		Tracer.setKeywordDisplayStatus(CallReceived.class, true);
+	}
+
+}
