0ac9f3579f5fb5ae48d57daded2faa1f411815c3
dewan <dewan@cs.unc.edu>
2019-03-16 12:35:11 -0400

    Changed 1099 to Registry.REGISTRY_PORT

--- src/examples/mvc/rmi/duplex/DistributedRMIRegistryStarter.java
+++ src/examples/mvc/rmi/duplex/DistributedRMIRegistryStarter.java
@@ -1,12 +1,13 @@
 package examples.mvc.rmi.duplex;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 
 public class DistributedRMIRegistryStarter {
 	public static void main (String[] args) {
 		try {
-			LocateRegistry.createRegistry(1099);
+			LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
 			Scanner scanner = new Scanner(System.in);
 			scanner.nextLine();
 		} catch (Exception e) {
