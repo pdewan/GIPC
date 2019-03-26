c65e2227d92cf2d277a53751787962538b46b26c
dewan <dewan@cs.unc.edu>
2019-03-26 11:06:11 -0400

    OpenMP cleanup

---
+++ src/examples/openmp/RandomDriver.java
@@ -0,0 +1,12 @@
+package examples.openmp;
+
+public class RandomDriver {
+	public static void main(String[] args) {
+		Random.setTrace(false);
+		ParallelRandom.setTrace(false);
+		System.out.println("Single threaded random");
+		Random.printRandom();
+		System.out.println("Multi threaded random");
+		ParallelRandom.printRandom();		
+	}
+}
